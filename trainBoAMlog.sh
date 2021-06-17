5efbdc457346b0592990ebf2dcc34c98947434d2
knonz <ouyang0329.cs04@g2.nctu.edu.tw>
2021-06-17 11:33:44 +0800

    exp manager from self.algo==hpg  to self.algo==hpo:

---
+++ trainBoAMlog.sh
@@ -0,0 +1,1 @@
+python train.py --algo hpo --classifier AM-log --env BreakoutNoFrameskip-v4 --tensorboard-log $PWD/tensorboard --hyperparams device:1
---
+++ trainBreakout.sh
@@ -0,0 +1,1 @@
+python train.py --algo hpo --classifier AM --env BreakoutNoFrameskip-v4 --tensorboard-log $PWD/tensorboard --hyperparams device:1
