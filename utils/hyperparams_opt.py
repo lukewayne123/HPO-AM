8e28ecd3ecdefb4eda40de2f64bcbf69fc25c61e
lukewayne123 <lukewayne123@gmail.com>
2021-07-09 09:43:11 +0800

    [FIX and ADD] Fix value calculation and Add tune hyperparamteres

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -7,6 +7,7 @@ from torch import nn as nn
 
 from utils import linear_schedule
 
+# 0708 check batch size, n_step,  lr, clip_range, vf_coef, activation fn
 def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     """
     Sampler for HPO hyperparams.
@@ -14,18 +15,21 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     :param trial:
     :return:
     """
-    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64, 128, 256, 512])
-    n_steps = trial.suggest_categorical("n_steps", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    batch_size = trial.suggest_categorical("batch_size", [128, 256, 512])
+    n_steps = trial.suggest_categorical("n_steps", [64, 128, 256, 512])
+    #gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99])
+    #gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
     learning_rate = trial.suggest_loguniform("lr", 1e-5, 1)
-    lr_schedule = "constant"
+    #lr_schedule = "constant"
     # Uncomment to enable learning rate schedule
-    # lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
-    ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
+    lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
+    #ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
     clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
-    n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
-    gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
-    max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
+    #n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10])
+    #n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
+    #gae_lambda = trial.suggest_categorical("gae_lambda", [0.9, 0.95, 0.98, 0.99])
+    #gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
+    #max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
     vf_coef = trial.suggest_uniform("vf_coef", 0, 1)
     net_arch = trial.suggest_categorical("net_arch", ["small", "medium"])
     # Uncomment for gSDE (continuous actions)
@@ -57,13 +61,13 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     return {
         "n_steps": n_steps,
         "batch_size": batch_size,
-        "gamma": gamma,
+        #"gamma": gamma,
         "learning_rate": learning_rate,
-        "ent_coef": ent_coef,
+        #"ent_coef": ent_coef,
         "clip_range": clip_range,
-        "n_epochs": n_epochs,
-        "gae_lambda": gae_lambda,
-        "max_grad_norm": max_grad_norm,
+        #"n_epochs": n_epochs,
+        #"gae_lambda": gae_lambda,
+        #"max_grad_norm": max_grad_norm,
         "vf_coef": vf_coef,
         # "sde_sample_freq": sde_sample_freq,
         "policy_kwargs": dict(
@@ -493,4 +497,5 @@ HYPERPARAMS_SAMPLER = {
     "tqc": sample_tqc_params,
     "ppo": sample_ppo_params,
     "td3": sample_td3_params,
+    "hpo": sample_hpo_params,
 }
