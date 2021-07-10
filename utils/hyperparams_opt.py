4a9b74d77d097c284efc6d173941eae942782c04
lukewayne123 <lukewayne123@gmail.com>
2021-07-10 19:41:18 +0800

    [LOG] Can train version, but wrong implement Need to change to pxq case

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -15,8 +15,9 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     :param trial:
     :return:
     """
+    alpha = trial.suggest_uniform("alpha", 0.1, 1)
     batch_size = trial.suggest_categorical("batch_size", [128, 256, 512])
-    n_steps = trial.suggest_categorical("n_steps", [64, 128, 256, 512])
+    n_steps = trial.suggest_categorical("n_steps", [64, 128, 256])
     #gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99])
     #gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
     learning_rate = trial.suggest_loguniform("lr", 1e-5, 1)
@@ -24,11 +25,12 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     # Uncomment to enable learning rate schedule
     lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
     #ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
-    clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
+    #clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
     #n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10])
     #n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
     #gae_lambda = trial.suggest_categorical("gae_lambda", [0.9, 0.95, 0.98, 0.99])
     #gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
+    #max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
     #max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
     vf_coef = trial.suggest_uniform("vf_coef", 0, 1)
     net_arch = trial.suggest_categorical("net_arch", ["small", "medium"])
@@ -40,7 +42,7 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
     ortho_init = False
     # ortho_init = trial.suggest_categorical('ortho_init', [False, True])
     # activation_fn = trial.suggest_categorical('activation_fn', ['tanh', 'relu', 'elu', 'leaky_relu'])
-    activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
+    activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu", "elu"])
 
     # TODO: account when using multiple envs
     if batch_size > n_steps:
@@ -64,15 +66,16 @@ def sample_hpo_params(trial: optuna.Trial) -> Dict[str, Any]:
         #"gamma": gamma,
         "learning_rate": learning_rate,
         #"ent_coef": ent_coef,
-        "clip_range": clip_range,
+        #"clip_range": clip_range,
         #"n_epochs": n_epochs,
         #"gae_lambda": gae_lambda,
         #"max_grad_norm": max_grad_norm,
         "vf_coef": vf_coef,
         # "sde_sample_freq": sde_sample_freq,
+        "alpha": alpha,
         "policy_kwargs": dict(
             # log_std_init=log_std_init,
-            net_arch=net_arch,
+            #net_arch=net_arch,
             activation_fn=activation_fn,
             ortho_init=ortho_init,
         ),
