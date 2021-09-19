1a4dfe22f3701e4215666d347b49d7fa580325f5
何國豪 <lukewayne123@CGI06.cgilab.nctu.edu.tw>
2021-09-19 10:02:23 +0800

    [ADD] Add training script and seaquest config

---
+++ trainMiniBreakoutBase.sh
@@ -0,0 +1,2 @@
+python train.py --algo $1 --seed $2 --env mini-breakout-v4 --tensorboard-log $PWD/tensorboards --hyperparams device:$3
+#python train.py --algo $1 --classifier $2 --aece WAE --seed $3 --env mini-breakout-v4 --tensorboard-log $PWD/tensorboard --hyperparams device:1
