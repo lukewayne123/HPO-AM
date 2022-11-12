d11ee0ec590420be7b2212944a9f530c3cf1e456
knonz <ouyang0329.cs04@g2.nctu.edu.tw>
2022-11-12 16:10:40 +0800

    plot update

--- plot/dplotablation.py
+++ plot/dplotablation.py
@@ -17,7 +17,15 @@ import matplotlib.ticker as ticker
 # root_folder = 'acrobotfr10sub789'
 # root_folder_list = ['lunarlander4Mfr10','lunarlander4Mfr20','lunarlander4Mfr30']
 # root_folder_list = ['acrobotfr10sub789','acrobot200epoch','acrobot30']
-root_folder_list = ['acrobotfr10sub789','acrobotfr20re','acrobot30']
+# root_folder_list = ['acrobotfr10sub789','acrobotfr20re','acrobot30']
+# root_folder_list = ['Cartpole200Epoch']
+# comapare_folder = ["lunarlander_epsiloncompare"]
+# comapare_folder = ["acrobot_epsiloncompare"]
+# comapare_folder = ["lunarlander_sptthresholdcompare_fr20_epsilon1"]
+# root_folder_list = ["lunarlander_sptthresholdcompare_fr20_epsilon1"]
+root_folder_list = ["acrobot_sptthresholdcompare"]
+# root_folder_list = ["acrobotfr20reepsilon1","acrobotepsilon2","acrobotepsilon3"]
+# root_folder_list = ["lunarlander4Mfr20epsilon1","sptLunarlander20epsilon2","sptLunarlander20epsilon3"]
 # gamename = 'cartpole'
 # gamename = 'mountaincar'
 # gamename = 'Cartpole'
@@ -27,7 +35,9 @@ gamename = 'Acrobot-v1'
 # gamename = 'MountainCar-v0'
 
 # folder_list = ['vanilla','spt090']
-folder_list = ['vanilla','spt90']
+# folder_list = ['vanilla','spt90']
+folder_list = ['vanilla','spt90','spt80','spt70']
+# folder_list = ['vanilla','spt90','vanilla2','spt902']
 merge_flag = True
 std_flag = True
 # std_flag = False
@@ -48,6 +58,7 @@ median_flag = False
 seed_list = ['123','285','789','78949','16842464' ] #lunarlander 4M fr10
 # DataLength = [1] * len(seed_list)
 def load_csv(file_path, x_scale = 1):
+    print("file_path",file_path)
     with open(file_path,'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         x = []
@@ -137,8 +148,10 @@ for root_folder in root_folder_list:
         Folder =  folder_list[idx]
         samples = []
         autopath = './{0}/{1}/'.format(root_folder ,Folder)
+        # autopath = './{2}/{0}/{1}/'.format(root_folder ,Folder,comapare_folder[0])
         dirs = os.listdir( autopath )
         for x in dirs:
+            # samples.append(load_csv("{3}/{0}/{1}/{2}".format(root_folder ,Folder, x,comapare_folder[0] ), x_scale=1))
             samples.append(load_csv("{0}/{1}/{2}".format(root_folder ,Folder, x ), x_scale=1))
         # for x in range( len(seed_list) ):
             # samples.append(load_csv("{0}/{1}/{2}.csv".format(root_folder ,Folder, seed_list[x] ), x_scale=1))
@@ -160,11 +173,21 @@ for root_folder in root_folder_list:
 # print("data_sets",data_sets)
 # for idx in range( len(folder_list) )
 # legend_list = [ 'vanilla CE HPO','CE HPO with SPT ignore pi(s,a)>0.9 state action pair' ]
-legend_list = ['vanilla with 0.1 uniform flipping of advantage signs','SPT with 0.1 uniform flipping of advantage signs','vanilla with 0.2 uniform flipping of advantage signs','SPT with 0.2 uniform flipping of advantage signs','vanilla with 0.3 uniform flipping of advantage signs','SPT with 0.3 uniform flipping of advantage signs']
+legend_list = [ 'vanilla CE HPO','CE HPO with SPT ignore pi(s,a)>0.9 state action pair','CE HPO with SPT ignore pi(s,a)>0.8 state action pair','CE HPO with SPT ignore pi(s,a)>0.7 state action pair' ]
+# legend_list = [ 'vanilla with epsilon=0.1','SPT with epsilon=0.1','vanilla with epsilon=0.2','SPT with epsilon=0.2','vanilla with epsilon=0.3','SPT with epsilon=0.3' ]
+# legend_list = ['vanilla with 0.1 uniform flipping of advantage signs','SPT with 0.1 uniform flipping of advantage signs','vanilla with 0.2 uniform flipping of advantage signs','SPT with 0.2 uniform flipping of advantage signs','vanilla with 0.3 uniform flipping of advantage signs','SPT with 0.3 uniform flipping of advantage signs']
 for i in range(len(data_sets)):
     if merge_flag:
-        ax.fill_between(data_sets[i][0], data_sets[i][2], data_sets[i][3], alpha=0.25, linewidth=0, color=colors[i ])
-        ax.plot(data_sets[i][0], data_sets[i][1], linewidth=3, color=colors[i ])
+        if gamename == 'CartPole-v1':
+            ax.fill_between(data_sets[i][0], data_sets[i][2], data_sets[i][3], alpha=0.25, linewidth=0, color=colors[i%2 ])
+            ax.plot(data_sets[i][0], data_sets[i][1], linewidth=3, color=colors[i%2 ])
+        else:
+            # ax.fill_between(data_sets[i][0], data_sets[i][2], data_sets[i][3], alpha=0.25, linewidth=0, color=colors[i ])
+            # colori = i if i <5 else i+1
+            colori = i 
+            ax.fill_between(data_sets[i][0], data_sets[i][2], data_sets[i][3], alpha=0.25, linewidth=0, color=colors[ colori ])
+            ax.plot(data_sets[i][0], data_sets[i][1], linewidth=3, color=colors[ colori ])
+        # ax.plot(data_sets[i][0], data_sets[i][1], linewidth=3, color=colors[i ])
     else:
         # for x in range( len(seed_list) ):
         for x in range( len(data_sets[i]) ):
@@ -189,20 +212,23 @@ ticks = ticker.FuncFormatter(lambda x, pos: '{0:g} '.format(x*scale))
 ax.xaxis.set_major_formatter(ticks)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
-# ax.set_ylim([0, 510])
+# ax.set_ylim([0, 510]) # for cartpole max rewards
 # plt.ylabel('average returns of 100 eval ', fontsize=25)
 plt.ylabel('average returns of policy ', fontsize=25)
 plt.xlabel('timesteps ', fontsize=25)
 # plt.title('NN policy + uniform flipping of advantage signs', fontsize=25)
 # plt.title(gamename+' full sa with 0.2 uniform flipping of advantage signs  ', fontsize=25)
 # plt.title(gamename+' with 0.2 uniform flipping of advantage signs  ', fontsize=25)
-plt.title(gamename+' with 10%,20%,30% uniform flipping of advantage signs  ', fontsize=25)
+plt.title(gamename+' with threshold1 = 0.9,0.8,0.7 ', fontsize=25)
+# plt.title(gamename+' with 10%,20%,30% uniform flipping of advantage signs  ', fontsize=25)
 # plt.title(gamename+' full sa with 0.45 uniform flipping of advantage signs  ', fontsize=25)
 # plt.set_size_inches(1400,890)
 # plt.show()
+plt.savefig('./full_sa_spt090_ablation2_{rootfoldername}_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(rootfoldername = root_folder_list[0] ,gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
+# plt.savefig('./full_sa_spt090_ablation2_{rootfoldername}_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(rootfoldername = comapare_folder[0] ,gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
 # plt.savefig('./full_sa_spt090_200epoch_adv020flip_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
 # plt.savefig('./full_sa_spt090_20epoch_adv010flip_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
-plt.savefig('./full_sa_spt090_ablation2_{rootfoldername}_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(rootfoldername = root_folder ,gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
+# plt.savefig('./full_sa_spt090_ablation2_{rootfoldername}_{gamename}_merge{mergeflag}_median{medianflag}_std{stdFlag}.png'.format(rootfoldername = root_folder ,gamename = gamename, mergeflag = merge_flag, medianflag= median_flag, stdFlag= std_flag ), format='png' )
 # plt.savefig('./full_sa_spt080_slt010_adv040flip_{gamename}.png'.format(gamename = gamename), format='png' )
 # plt.savefig('./full_sa_spt060_adv040flip_moreEpoch_{gamename}.png'.format(gamename = gamename), format='png' )
 # plt.savefig('./full_sa_spt060_adv045flip_moreEpoch_{gamename}.png'.format(gamename = gamename), format='png' )
